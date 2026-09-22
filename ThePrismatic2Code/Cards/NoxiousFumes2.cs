f7780318b846c0303baeb9f4b80a7cc9e45c8ef0
adamdickey <adickey00@gmail.com>
2026-09-21 20:18:47 -0400

    Fixed Noxious Fumes cost

--- ThePrismatic2Code/Cards/NoxiousFumes2.cs
+++ ThePrismatic2Code/Cards/NoxiousFumes2.cs
@@ -12,7 +12,7 @@ using ThePrismatic2.ThePrismatic2Code.Powers;
 namespace ThePrismatic2.ThePrismatic2Code.Cards;
 
 [Pool(typeof(ThePrismatic2CardPool))]
-public class NoxiousFumes2() : ThePrismatic2Card(2, 
+public class NoxiousFumes2() : ThePrismatic2Card(1, 
     CardType.Power, CardRarity.Uncommon, 
     TargetType.Self)
 {
